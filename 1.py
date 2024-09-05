0eaf55eb3325c574a7f54235ee711249fef29872
BanChangGo <96645133+BanChangGo@users.noreply.github.com>
2024-09-05 14:56:39 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,20 +2,15 @@ import gpiod
 import time
 
 
-chip = gpiod.Chip('gpiochip0') 
+chip = gpiod.Chip('gpiochip0')  
 line = chip.get_line(17)  
 
-config = gpiod.line_request()
-config.consumer = 'button-monitor'
-config.request_type = gpiod.line_request.DIRECTION_INPUT
-config.flags = gpiod.line_request.FLAG_BIAS_PULL_UP 
 
-
-line.request(config)
+line.request(consumer='button-monitor', type=gpiod.LINE_REQ_DIR_IN, flags=[gpiod.LINE_REQ_FLAG_BIAS_PULL_UP])
 
 try:
     while True:
-    
+        
         button_state = line.get_value()
         
         if button_state == 0: 
@@ -23,11 +18,11 @@ try:
         else:
             print("Button Released!")
         
-        time.sleep(0.1)
+        time.sleep(0.1) 
 
 except KeyboardInterrupt:
     
     print("Exiting...")
 
 finally:
-    line.release()  
+    line.release() 
