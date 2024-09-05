94307cc87614fd65c43dcb5c67b8e01c9aa78fa8
BanChangGo <96645133+BanChangGo@users.noreply.github.com>
2024-09-05 14:52:19 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,28 +1,33 @@
-import RPi.GPIO as GPIO  
-import time 
+import gpiod
+import time
 
 
-BUTTON_PIN = 17
+chip = gpiod.Chip('gpiochip0') 
+line = chip.get_line(17)  
+
+config = gpiod.line_request()
+config.consumer = 'button-monitor'
+config.request_type = gpiod.line_request.DIRECTION_INPUT
+config.flags = gpiod.line_request.FLAG_BIAS_PULL_UP 
 
 
-GPIO.setmode(GPIO.BCM)  
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+line.request(config)
 
 try:
     while True:
+    
+        button_state = line.get_value()
         
-        button_state = GPIO.input(BUTTON_PIN)
-        
-        if button_state == GPIO.LOW: 
+        if button_state == 0: 
             print("Button Pressed!")
         else:
             print("Button Released!")
         
-        time.sleep(0.1) 
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
-   
+    
     print("Exiting...")
 
 finally:
-    GPIO.cleanup()  
+    line.release()  
