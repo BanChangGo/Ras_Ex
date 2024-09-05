b6da8fc9f564e19f0fbde2db8d066e629ac7e594
BanChangGo <96645133+BanChangGo@users.noreply.github.com>
2024-09-05 15:57:33 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,27 +1,22 @@
 from gpiozero import OutputDevice
 from time import sleep
 
-RELAY_PIN = 17  
-SOLENOID_PIN = 18  
-
-
-relay = OutputDevice(RELAY_PIN)
-solenoid = OutputDevice(SOLENOID_PIN)
+# GPIO 32번 핀을 사용하여 릴레이를 제어합니다.
+solenoid = OutputDevice(32)
 
 try:
     while True:
-        relay.on()
-        sleep(2)  
-
+        # 솔레노이드 켜기
         solenoid.on()
-        sleep(2)  
-
+        print("솔레노이드 켜짐")
+        sleep(1)  # 1초 대기
+        
+        # 솔레노이드 끄기
         solenoid.off()
-        sleep(2)  
-
-        relay.off()
-        sleep(2) 
+        print("솔레노이드 꺼짐")
+        sleep(1)  # 1초 대기
 
 except KeyboardInterrupt:
-    relay.off()
+    # Ctrl+C로 프로그램 종료 시 핀을 정리합니다.
     solenoid.off()
+    print("프로그램 종료")
