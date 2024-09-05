2e8e54649cf1a08929bd8af8de682ef1fe197f55
BanChangGo <96645133+BanChangGo@users.noreply.github.com>
2024-09-05 14:38:07 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,7 @@
-import os
+import subprocess
 
-os.system("libcamera-still -t 10000")
+# libcamera-still 명령을 통해 10초 동안 미리보기
+try:
+    subprocess.run(["libcamera-still", "-t", "10000"], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred: {e}")
