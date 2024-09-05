fc04e5db7393ba063ffd4500c3ef24d242dc9fa0
BanChangGo <96645133+BanChangGo@users.noreply.github.com>
2024-09-05 14:58:34 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,20 +6,24 @@ chip = gpiod.Chip('gpiochip0')
 line = chip.get_line(17)  
 
 
-line.request(consumer='button-monitor', type=gpiod.LINE_REQ_DIR_IN, flags=[gpiod.LINE_REQ_FLAG_BIAS_PULL_UP])
+config = gpiod.LineRequest()
+config.consumer = 'button-monitor'  
+config.request_type = gpiod.LineRequest.DIRECTION_INPUT  
+config.flags = gpiod.LineRequest.FLAG_BIAS_PULL_UP 
+
+line.request(config)  
 
 try:
     while True:
         
         button_state = line.get_value()
         
-        if button_state == 0: 
+        if button_state == 0:
             print("Button Pressed!")
         else:
             print("Button Released!")
         
-        time.sleep(0.1) 
-
+        time.sleep(0.1)  
 except KeyboardInterrupt:
     
     print("Exiting...")
